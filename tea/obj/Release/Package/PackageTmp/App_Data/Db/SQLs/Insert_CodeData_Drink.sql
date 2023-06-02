f8ce60d5926ab196d96742e10289a74baba08ff0
jacky58_huang <jacky58.huang@gmail.com>
2023-06-02 19:14:46 +0800

    1120602-1914

---
+++ tea/obj/Release/Package/PackageTmp/App_Data/Db/SQLs/Insert_CodeData_Drink.sql
@@ -0,0 +1,23 @@
+USE tea
+
+SELECT *
+FROM CodeDatas
+WHERE 1 = 1
+AND BaseNo = 'Drink'
+
+/*
+
+IF EXISTS(SELECT TOP 1 1 FROM CodeDatas WHERE BaseNo = 'Drink')
+BEGIN
+	DELETE FROM CodeDatas
+	WHERE BaseNo = 'Drink'
+END
+
+INSERT INTO CodeDatas(IsEnabled, BaseNo, ParentNo, SortNo, CodeNo, CodeName, CodeValue, Remark)
+VALUES(1, 'Drink', '', '01', 'Fresh', '鮮榨原汁', '', '')
+INSERT INTO CodeDatas(IsEnabled, BaseNo, ParentNo, SortNo, CodeNo, CodeName, CodeValue, Remark)
+VALUES(1, 'Drink', '', '02', 'Tea', '鮮茶香茗', '', '')
+INSERT INTO CodeDatas(IsEnabled, BaseNo, ParentNo, SortNo, CodeNo, CodeName, CodeValue, Remark)
+VALUES(1, 'Drink', '', '03', 'Special', '獨家特調', '', '')
+
+*/
